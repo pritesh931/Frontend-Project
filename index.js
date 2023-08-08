9d74c0e0fc6bdc5d944dc3c51235f2d7e3fc2fe8
pritesh931 <111389677+pritesh931@users.noreply.github.com>
2023-08-08 11:15:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let accordions = document.querySelectorAll('.accordion')
+accordions.forEach((acco) => {
+    acco.onclick = () => {
+    accordions.forEach((subcontent) =>{
+        subcontent.classList.remove("active")
+    })
+        acco.classList.add("active")
+    }
+})
